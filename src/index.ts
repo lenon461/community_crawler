579920ca4254ca7b58c27f0b2e9904fb8487117d
ljs <lenon461@mgail.com>
2019-12-14 23:52:52 +0900

    :herb: 메인파일 정리

--- src/index.ts
+++ src/index.ts
@@ -5,47 +5,6 @@ import { Posts, Post, PostPayload } from './Post';
 import * as fs from 'fs';
 import levenshtein from 'fast-levenshtein'
 
-// export async function writeFile22(posts: Post[], site: selector): Promise<void>{
-    
-//     let curStartPageNumber = pageNumberOffset;
-//     let curEndPageNumber = 10;
-//     const file = `../data/${site.sitename}.json`
-    
-//     if (!fs.existsSync(file)) {
-//         fs.mkdirSync('../data');
-//         fs.writeFileSync(file, '\n');
-//     }
-    
-
-//     while(pageNumberLimit >= curEndPageNumber){
-//         const posts = await scrapyPosts(curStartPageNumber, curEndPageNumber, site);
-//         const obj = posts.map(post => {
-//             return {'payload': post}
-//         })
-//         fs.appendFileSync(file, JSON.stringify(obj)+ '\n');
-        
-//         console.log(curStartPageNumber +' page ~ ' + curEndPageNumber + ' page write End');
-//         curStartPageNumber += 10;
-//         curEndPageNumber += 10;
-//     }
-// }
-
-async function write() {
-    
-    try {
-
-        // await writeFile(1, 10, sites[0]); //dogdrip
-        // console.log('site1 write End');
-
-        // await writeFile(1, 10, sites[1]); //fmkorea
-        // console.log('site2 write End');
-        
-    } catch (error) {
-        
-    }
-}
-let i = 0
-
 async function compare(Posts1: Post[], Posts2: Post[]) {
     
     Posts1.map((post1: Post) => {
