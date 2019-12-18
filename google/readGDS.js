943e466f5c094d847e91f0f08910ef097d8c9c42
jslee02 <jslee02@korens.co.kr>
2019-12-18 13:58:54 +0900

    :cloud: GCD get data by query

--- google/readGDS.js
+++ google/readGDS.js
@@ -7,7 +7,8 @@ exports.readGDS = async (req, res) => {
             projectId: 'tribal-isotope-228803',
             keyFilename: 'tribal-isotope-228803-8d000bd04e4c.json'
         });
-        const kindName = ['posts', yyyymmdd(new Date())];
+        const keyName = req.query.key || yyyymmdd(new Date());
+        const kindName = ['posts', keyName];
         const key = datastore.key(kindName);
         datastore.get(key, function(err, entity) {
             res.status(200).send(entity);
